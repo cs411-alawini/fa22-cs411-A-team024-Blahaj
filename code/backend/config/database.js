5103def3d8b5fc0d3f7fc8ca9565e021815179de
Kevin Dsouza <dsouza.kevin952@gmail.com>
2022-11-11 23:54:55 -0600

    Add files via upload

---
+++ code/backend/config/database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    port:process.env.DB_PORT,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.MYSQL_DB,
+    connectionLimit:10
+});
+
+module.exports = pool.promise();
