cfa6a657ee0e12267e7f22461eadd6174824eb2e
Kevin Dsouza <dsouza.kevin952@gmail.com>
2022-12-02 23:16:00 -0600

    Add files via upload

---
+++ code/backend/auth/token_validation.js
@@ -0,0 +1,31 @@
+const { verify } = require("jsonwebtoken");
+
+module.exports = {
+    checkToken: (req,res,next) => {
+        let token = req.get("authorization");
+        if(token)
+        {
+            token = token.slice(7);
+            verify(token,process.env.JWT_AUTH,(err,decoded) => {
+                if(err){
+                    res.json({
+                        success:0,
+                        message:"Invalid Token"
+                    })
+                }
+                else
+                {
+                    next();
+                }
+
+            });
+
+        }
+        else{
+            res.json({
+                success:0,
+                message:"Access Denied! Unauthorized user."
+            })
+        }
+    }
+}
