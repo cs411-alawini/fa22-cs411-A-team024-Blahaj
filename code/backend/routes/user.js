8a978dd94168692486d8bc8fba168c99201c27f8
Kevin Dsouza <dsouza.kevin952@gmail.com>
2022-12-02 23:17:18 -0600

    Add files via upload

---
+++ code/backend/routes/user.js
@@ -0,0 +1,30 @@
+const { postUsers } = require("../controllers/user.js");
+const { getUsers } = require("../controllers/user.js");
+const { getUsersbyId } = require("../controllers/user.js");
+const { updateUsersbyId } = require("../controllers/user.js");
+const { getUsersUnivbyId } = require("../controllers/user.js");
+const { getUsersDoctorbyId } = require("../controllers/user.js");
+const { updateDoctor } = require("../controllers/user.js");
+const {insertFormVaccine} = require("../controllers/user.js");
+const { login } = require("../controllers/user.js");
+
+
+const router = require("express").Router();
+
+const { checkToken } = require("../auth/token_validation.js");
+
+router.post("/",postUsers);
+
+router.get("/",checkToken,getUsers);
+router.get("/:id",checkToken,getUsersbyId);
+router.get("/univ/:id",checkToken,getUsersUnivbyId);
+router.get("/doctor/:id",checkToken,getUsersDoctorbyId);
+router.get("/univ/:id",checkToken,getUsersUnivbyId);
+router.put("/update/:id",updateUsersbyId);
+router.post("/formVaccine",insertFormVaccine);
+
+
+router.post("/auth/login",login);
+
+
+module.exports = router; 
